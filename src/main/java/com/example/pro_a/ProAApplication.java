ada6678ad7b4f163652aec0f3daa79d48ba19537
Sumin <resia07@kyungmin.ac.kr>
2022-09-19 10:18:48 +0900

    project_create

---
+++ src/main/java/com/example/pro_a/ProAApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pro_a/ProAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pro_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
