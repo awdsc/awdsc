79cee4325bbdf1b83541a90d4c59414f28ed93ac
awdsc <65292603+awdsc@users.noreply.github.com>
2022-11-14 00:57:03 +0900

    dd

--- src/main/resources/static/assets/js/cartoonMakeIndex.js
+++ src/main/resources/static/assets/js/cartoonMakeIndex.js
@@ -10,6 +10,7 @@
 }
 function defaultClick(event)
 {
+
     let menu = document.getElementById("contextmenu");
     menu.style.display = "none";
     menu.style.top = null;
@@ -31,7 +32,7 @@ function createObject(type)
 let sY;
 let eX;
 let eY;
-
+let objCounter = 0;
 
 function saveS(event)
 {
@@ -67,28 +68,108 @@ function saveS(event)
          maker(bubbleType,sX,sY,eX,eY);
      }
  }
-function maker(bType,sx,sy,ex,ey)
+
+ function moveObj(id) {
+    let selectObj = document.getElementById(id);
+
+ }
+
+
+
+
+ function dbCreateSpeech(id) {
+
+     let speechDiv = document.getElementById(id);
+     let speech = document.createElement("a");
+     speech.innerText = "텍스트를 입력해 주세요.";
+     speech.style.textAlign = "center";
+     console.log(speechDiv.innerHTML);
+     console.log(speech.innerHTML);
+     speechDiv.appendChild(speech);
+     return function (){};
+ }
+ function maker(bType,sx,sy,ex,ey)
 {
     let sector = document.getElementById("createCut");
     let CM = document.createElement("div");
-    CM.style.position = "absolute";
+    CM.id = "div" +objCounter;
+    CM.style.position = "relative";
     CM.style.top = sy+'px';
     CM.style.left = sx +'px';
     CM.style.width = (ex-sx)+'px';
     CM.style.height = (ey-sy)+'px';
+    CM.style.zIndex = 3;
+    CM.draggable = true;
 
     let img = document.createElement("img")
     img.src = "../static/assets/img/"+bType +".png";
-    img.style.height = "inherit";
-    img.style.width = "inherit";
-    img.style.position = "fixed";
+    img.classList += "cimg";
+
+    let speech = document.createElement("textarea");
+    speech.innerText = "텍스트를 입력해 주세요.";
+    speech.classList += "ctextarea";
+    speech.focus = ()=> {speech.style.border = none;};
+
+    CM.appendChild(speech);
     CM.appendChild(img);
     sector.appendChild(CM);
+
+
+    objCounter ++;
     console.log("생성완료");
 }
+/*
+let container = document.querySelector("draggable");
+ container.addEventListener("mousedown", dragStart, false);
+ container.addEventListener("mouseup", dragEnd, false);
+ container.addEventListener("mousemove", drag, false);
+
+ function dragStart(e) {
+     if (e.type === "touchstart") {
+         initialX = e.touches[0].clientX - xOffset;
+         initialY = e.touches[0].clientY - yOffset;
+     } else {
+         initialX = e.clientX - xOffset;
+         initialY = e.clientY - yOffset;
+     }
+
+     if (e.target === dragItem) {
+         active = true;
+     }
+ }
+
+ function dragEnd(e) {
+     initialX = currentX;
+     initialY = currentY;
+
+     active = false;
+ }
+
+ function drag(e) {
+     if (active) {
+
+         e.preventDefault();
+
+         if (e.type === "touchmove") {
+             currentX = e.touches[0].clientX - initialX;
+             currentY = e.touches[0].clientY - initialY;
+         } else {
+             currentX = e.clientX - initialX;
+             currentY = e.clientY - initialY;
+         }
+
+         xOffset = currentX;
+         yOffset = currentY;
+
+         setTranslate(currentX, currentY, dragItem);
+     }
+ }
+*/
 
 document.addEventListener('contextmenu',rightClick,false);
 document.addEventListener('click',defaultClick,false);
+
+
 document.onmousedown = saveS;
 document.onmouseup = saveE;
 
